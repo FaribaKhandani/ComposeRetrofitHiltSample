3c8b82620af3a36e89c9a6bc3ce9a159b0eb68bf
FaribaKhandani <your_email_address@example.com>
2024-03-25 17:11:34 +0100

    added data class

---
+++ app/src/main/java/com/example/composeretrofithiltsample/api/ModelApi.kt
@@ -0,0 +1,18 @@
+package com.example.composeretrofithiltsample.api
+
+data class ModelApi (
+    val id: Int,
+    val imageUrl: String,
+    val firstName: String,
+    val lastName: String,
+    val stickers: List<String>,
+    val gender: String,
+    val phoneNumber: String,
+        )
+
+
+
+
+
+
+
--- app/src/main/java/com/example/composeretrofithiltsample/viewmodel/ViewModelApi.kt
+++ app/src/main/java/com/example/composeretrofithiltsample/viewmodel/ViewModelApi.kt
@@ -1,6 +1,10 @@
 package com.example.composeretrofithiltsample.viewmodel
 
 import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
 
-class ViewModelApi :ViewModel() {
+
+@HiltViewModel
+class ViewModelApi
+    :ViewModel() {
 }
